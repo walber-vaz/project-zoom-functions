e83f2876c287a004d666623373ba931eb8e06273
Walber Vaz da Silva <wvs.walber@gmail.com>
2022-10-31 16:13:50 -0300

    2. Implemente a função getAnimalsOlderThan - Feito

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,14 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const getNameAge = species.find((specie) => specie.name === animal);
+  /**
+   * @documents => MDN
+   * O método every() testa se todos os elementos do array passam pelo teste implementado pela função fornecida. Este método retorna um valor booleano.
+   */
+  return getNameAge.residents.every((resident) => resident.age >= age);
 }
 
+console.log(getAnimalsOlderThan('otters', 7));
+console.log(getAnimalsOlderThan('penguins', 10));
 module.exports = getAnimalsOlderThan;
