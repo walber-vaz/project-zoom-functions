6a1f57dff138f0b7733c3848a326b7e3439a3158
Walber Vaz da Silva <wvs.walber@gmail.com>
2022-10-31 23:08:23 -0300

    11 - Implemente a função getEmployeesCoverage para retornar as informações sobre a pessoa colaboradora e por quais espécies ela é responsável - Feito

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,41 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
-}
+const { employees, species } = data;
+
+const getEmployee = (nameEmployee) => {
+  if (nameEmployee.name) {
+    return employees.find(({ firstName, lastName, id }) =>
+      [firstName, lastName].includes(nameEmployee.name || nameEmployee.id));
+  }
+
+  if (nameEmployee.id) return employees.find(({ id }) => id === nameEmployee.id);
+};
+
+const confirmEmployee = (name) => {
+  const isError = getEmployee(name);
+  if (!isError) throw new Error('Informações inválidas');
+  const responsibility = species.filter((specie) => isError.responsibleFor.includes(specie.id));
+
+  return {
+    id: isError.id,
+    fullName: `${isError.firstName} ${isError.lastName}`,
+    species: responsibility.map((specie) => specie.name),
+    locations: responsibility.map((specie) => specie.location),
+  };
+};
+
+const getEmployeesCoverage = (name) => {
+  if (!name) {
+    const arrNameResponsability = [];
+    employees.forEach((employee) => {
+      const firstNameEmployee = {
+        name: employee.firstName,
+      };
+      arrNameResponsability.push(confirmEmployee(firstNameEmployee));
+    });
+    return arrNameResponsability;
+  }
+  return confirmEmployee(name);
+};
 
 module.exports = getEmployeesCoverage;
