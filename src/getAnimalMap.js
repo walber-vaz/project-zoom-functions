496f73b565a0a97154d048c89389937c796295c9
Walber Vaz da Silva <wvs.walber@gmail.com>
2022-11-01 15:26:13 -0300

    Resuisito 14 em desenvolvimento

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,30 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
+// https://pt.stackoverflow.com/questions/16483/remover-elementos-repetido-dentro-de-um-array-em-javascript
+// const getLocation = () => {
+//   const mapLocation = species.map(({ location }) => location);
+//   return [...new Set(mapLocation)];
+// };
+
+const filterAnimal = (loc) => species
+  .filter((specie) => specie.location === loc)
+  .map(({ name }) => name);
+
+// const namesAnimals = () => species
+//   .filter((specie) => specie.residents)
+//   .map(({ residents }) => residents.map((obj) => obj.name));
+
+const getDefaultReturn = () => species.reduce((acc, el) => {
+  acc[el.location] = filterAnimal(el.location);
+  return acc;
+}, {});
+
 function getAnimalMap(options) {
-  // seu código aqui
+  if (options === undefined) return getDefaultReturn();
+  if (options.sex) return getDefaultReturn();
 }
 
+console.log(getAnimalMap());
 module.exports = getAnimalMap;
